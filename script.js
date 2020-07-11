799c3f82aa314ea29b8ac4417e67918f3c7ff253
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-07-11 20:05:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = () =>{
+
+
+
+
+var cardOne = document.getElementById("cardOne");
+
+var playerPage = document.getElementById("playerPage");
+
+cardOne.onclick = () => {
+
+playerPage.style.transform = "translateY(0%)";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
